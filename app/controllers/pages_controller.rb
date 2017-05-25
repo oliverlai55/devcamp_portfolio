1c7249c8d8f30bfb69eb71aee679fd9775918b6f
oliverlai55 <oliverlai55@gmail.com>
2017-05-25 00:54:36 -0400

    Integrate skills via model generator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     # the @ is an instance variable, the Blog is in models
     @posts = Blog.all
+    @skills = Skill.all
   end
 
   def about
